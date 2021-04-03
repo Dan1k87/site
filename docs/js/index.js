146f7cea1614c29c9f36506d254e3669d6150ffc
Dan1k87 <81391995+Dan1k87@users.noreply.github.com>
2021-04-04 00:48:11 +0400

    Add files via upload

--- docs/js/index.js
+++ docs/js/index.js
@@ -10,21 +10,47 @@ $(document).ready(function(){
 	});
 
 	$('.product-list-slider').slick({
-		accessibility: false,
-		arrows: false,
+		infinite: false,
+		accessibility: true,
+		arrows: true,
 		slidesToShow: 3,
 		slidesToScroll: 3,
 		rows: 2,
-		slidesPerRow:1,
 		dots: true,
+		prevArrow: $('.product-slider-arrows__arrow_prev'),
+		nextArrow: $('.product-slider-arrows__arrow_next'),
 		responsive:[
 			{
 				breakpoint: 1070,
 				settings: {
 					slidesToShow: 2
 				}
+			},
+			{
+				breakpoint: 510,
+				settings: {
+					slidesToShow: 1,
+					slidesToScroll: 1,
+					rows: 1,
+				}
 			}
 		]
+	});
+
+
+	$('.product-list-slider').find('.slick-dots').find('li').each(function(index){
+		if($(this).hasClass('slick-active')){
+			$('.product-slider-arrows__current').text(index + 1);
+		}
+	});
+	$('.product-slider-arrows__total').text($('.product-list-slider').find('.slick-dots').find('li').length);
+
+	$('.product-list-slider').on('afterChange', function(event, slick, currentSlide){
+		$(this).find('.slick-dots').find('li').each(function(index){
+			if($(this).hasClass('slick-active')){
+				$('.product-slider-arrows__current').text(index + 1);
+			}
+		});
 	});
 
 });
